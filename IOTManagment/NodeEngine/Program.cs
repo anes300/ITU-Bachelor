6231d50918867bef0c9ca37ba4956d80313fffab
Mikkel-MJ <mikkel1260@gmail.com>
2022-02-25 10:49:44 +0100

    removed test

--- IOTManagment/NodeEngine/Program.cs
+++ IOTManagment/NodeEngine/Program.cs
@@ -4,10 +4,7 @@ using System.Text.Json;
 
 Console.WriteLine("Hello, World!");
 
-//string test = "Select temp, Sum(cpu) Interval 50 Where temp > 50 && (cpu < 40 || temp > 40 || cpu = 50)";
 string test = "Select temp, Sum(cpu) Interval 50 Where (temp > 50) && (cpu < 40 || temp > 40 || cpu = 50)";
-//string test2 = "(temp > 50 && cpu < 40 || temp > 40 || cpu = 50) ";
-//string test = "Select temp, Sum(cpu) Interval 50 Where temp > 50";
 
 QueryParser parser = new QueryParser();
 string x = JsonSerializer.Serialize(parser.ParserQuery(test));
