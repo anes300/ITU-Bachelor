e9720adf484721c9e81157ee78b894c2465c1cd8
Mikkel Møller Jensen <momj@itu.dk>
2022-03-09 15:05:43 +0100

    added logging in NodeEngine and added Query scheduler

---
+++ IOTManagment/NodeEngine/Jobs/QueryExecutionJob.cs
@@ -0,0 +1,27 @@
+﻿using Model.Queries;
+using Model.Queries.Statements;
+using Quartz;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace NodeEngine.Jobs
+{
+    public class QueryExecutionJob : IJob
+    {
+        
+        public async Task Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            SelectStatement? selectStatement = JsonSerializer.Deserialize<SelectStatement>(dataMap.GetString("Select"));
+            WhereStatement? whereStatement = JsonSerializer.Deserialize<WhereStatement>(dataMap.GetString("Where"));
+
+            Log.Logger.Information("Log Something");
+            await Console.Error.WriteLineAsync("Select variable: " + selectStatement.Variables[0].Variable);
+        }
+    }
+}
--- IOTManagment/NodeEngine/Program.cs
+++ IOTManagment/NodeEngine/Program.cs
@@ -1,11 +1,45 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Services;
 using System.Text.Json;
+using NodeEngine.Jobs;
+using Model.Queries;
+using Model.Queries.Statements;
+using NodeEngine.Service;
+using Serilog;
+using Microsoft.Extensions.Logging;
 
-Console.WriteLine("Hello, World!");
+// Setup logger
+var log = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .CreateLogger();
+Log.Logger = log;
 
-string test = "Select temp, Sum(cpu) Interval 50 Where (temp > 50) && (cpu < 40 || temp > 40 || cpu = 50)";
+// create log factory for scheduler
+var logFactory = new LoggerFactory()
+    .AddSerilog(log);
+// sets the logger for the Scheduler 
+Quartz.Logging.LogContext.SetCurrentLogProvider(logFactory);
+QueryScheduler scheduler = new QueryScheduler();
+Console.WriteLine("Query Engine Started");
+
+string test = "Select temp, Sum(cpu) Interval 1000 Where (temp > 50) && (cpu < 40 || temp > 40 || cpu = 50)";
+
+string test2 = "Select CPU, Sum(cpu) Interval 1000 Where (temp > 50) && (cpu < 40 || temp > 40 || cpu = 50)";
+
+
+log.Information("");
 
 QueryParser parser = new QueryParser();
-string x = JsonSerializer.Serialize(parser.ParserQuery(test));
 Console.WriteLine(JsonSerializer.Serialize(parser.ParserQuery(test)));
+var query = parser.ParserQuery(test);
+var query2 = parser.ParserQuery(test2);
+
+await scheduler.AddQueryJobAsync(query2);
+await scheduler.AddQueryJobAsync(query);
+
+await Task.Delay(TimeSpan.FromSeconds(10));
+Console.WriteLine("Removing query 2");
+await scheduler.RemoveQueryjobAsync(query2.Id);
+await Task.Delay(TimeSpan.FromSeconds(10));
+
